5381d5c2b28d83b255b5813ca4db96504ebb7b79
KATILYN WIGGINS <KATILYNSWIGGINS@GMAIL.COM>
2021-06-04 13:26:28 -0400

    added api functionality

--- src/components/characters/CharacterItem.tsx
+++ src/components/characters/CharacterItem.tsx
@@ -1,1 +1,36 @@
+import React from 'react'; 
+import { Link } from 'react-router-dom'
+import styles from './CharacterItem.module.css'; 
 
+//typescript wants you to type your props
+interface Props {
+  id: number;
+  name: string;
+  imageUrl: string;
+}
+
+//define component and the props it is taking in -- reminds me of proptypes 
+//React.FC - react functional component -- typescript notation 
+
+const CharacterItem: React.FC<Props> = ({
+  id, name, imageUrl
+}) => {
+ return (
+   <div>
+     <Link
+      to={`/detail/${id}`}
+      className={styles.link}
+     >
+       <figure>
+         <img 
+         src={imageUrl}
+         alt={name}
+         className={styles.img}></img>
+         <figcaption>{name}</figcaption>
+       </figure>
+     </Link>
+   </div>
+ )
+}
+
+export default CharacterItem; 
--- src/services/trek-api.ts
+++ src/services/trek-api.ts
@@ -1,1 +1,18 @@
+//in typescript you want to tell your functions what they are expecting to return 
+// go to types folder and make a type (interface) for it 
+// import the interface 
+//use brackets/array notation to expect more than one 'character'
+//in create-react-app .env everything must start with REACT_APP
+//must declare url as string 
 
+import { Character } from '../types/types'; 
+const url = process.env.REACT_APP_API_URL as string
+
+export const getCharacters = async(): Promise<Character[]> => {
+  const res = await fetch(url); 
+  const json = await res.json(); 
+
+  if(!res.ok) throw new Error('EEEEEERORRRRR');
+  
+  return json; 
+}
